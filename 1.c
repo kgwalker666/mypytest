66183e8aec1465d97419f9d1aeba3ab5870c5bc0
kgwalker <kgwalker@163.com>
2020-07-21 17:15:59 +0800

    添加1.c

---
+++ 1.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: 1.c
+    > Author: kgwalker
+    > Mail: kgwalker@163.com
+    > Created Time: 2020年07月21日 星期二 17时13分15秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	printf("hello...\n");
+	return 0;
+}
